d0fbbe2cda0e97462248e1f04526c4cbe0008a1c
hushuo1207 <1253740807@qq.com>
2021-06-26 16:20:31 +0800

    init gshop

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <router-view></router-view>
+    <FooterGuide v-show="$route.meta.showFooter"/>
+  </div>
+</template>
+
+<script>
+import FooterGuide from './components/FooterGuide/FooterGuide.vue'
+
+export default {
+  //name: 'App'
+  components: {
+    FooterGuide
+  }
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  @import './common/stylus/mixins.styl' 
+  .app
+    width 100%
+    height 100%
+    background #f5f5f5
+</style>
